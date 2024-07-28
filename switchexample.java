ae3a56fe0427a4b8fa922690af2350d6855133aa
nusrathParveen1729 <152918898+nusrathParveen1729@users.noreply.github.com>
2024-07-28 20:18:25 +0530

    Add files via upload

---
+++ switchexample.java
@@ -0,0 +1,37 @@
+public class switchexample
+{
+    public static void main(String a[])
+    {
+        int n = 0;
+        switch(n)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("sUNDAY");
+                break;
+            default:
+                System.out.println("hehe");
+        }
+    }
+}
+    
+// the momeemnt its matching the case it will execute all the other blcoks 
+//break is imp to come out of the block 
+//default
