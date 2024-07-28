d1cd282dcdda7b822e6e9a5f4770ba942329dd9d
nusrathParveen1729 <152918898+nusrathParveen1729@users.noreply.github.com>
2024-07-28 20:06:17 +0530

    Add files via upload

---
+++ ifElseStatements.java
@@ -0,0 +1,26 @@
+public class ifElseStatements
+{
+    public static void main(String a[]) 
+    {
+        int x = 98;
+        if(x>10 && x<=20)
+        System.out.println("HELLO");
+        else
+        System.out.println("BYE");
+
+
+        //greatest number 
+        int d = 5;
+        int b = 7;
+        if(d>b)
+        {
+            System.out.println(d);
+            System.out.println("HELLO");
+        }
+      
+        else
+        System.out.println(b);
+
+        
+    }
+}
